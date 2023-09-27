cba411ad48b10946308d4f8061c9387c0ddac83f
Yeray Gutiérrez Cedrés <yeray.gutierrez@suse.com>
2023-09-27 16:51:16 +0100

    fix

--- move_issue.py
+++ move_issue.py
@@ -1,24 +1,21 @@
 import os
 from github import Github
 
-# Use GitHub token from environment variable
 g = Github(os.environ['GITHUB_TOKEN'])
 
-repo_A = g.get_repo("owner/repoSource")  # Replace 'owner' with the appropriate owner/organization name
-repo_B = g.get_repo("owner/repoTarget")  # Replace 'owner' with the appropriate owner/organization name
+repo_A = g.get_repo("owner/repoOrigin") 
+repo_B = g.get_repo("owner/repoTarget")
 
 issue_number = int(os.environ['ISSUE_NUMBER'])
 issue = repo_A.get_issue(issue_number)
 
 if "easy" in [label.name for label in issue.get_labels()]:
-    # Create a new issue in repo B with the same content
     new_issue = repo_B.create_issue(
         title=issue.title,
         body=f"Originally reported by: {issue.user.login}\n\n{issue.body}",
-        labels=[label.name for label in issue.get_labels() if label.name != "easy"]  # Copy labels excluding "easy"
+        labels=[label.name for label in issue.get_labels() if label.name != "good first issue"] 
     )
     
-    # Close the original issue in repo A with a comment
     issue.create_comment(f"This issue has been moved to {new_issue.html_url}.")
     issue.edit(state="closed")
 
